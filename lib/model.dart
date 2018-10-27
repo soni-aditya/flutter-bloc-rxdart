ab7d4abc460a408f0c587169a4dfbc82a0ce8531
Aditya Soni <adityasoni5859@gmail.com>
2018-10-27 21:24:27 +0530

    feature : Added model.

--- lib/model.dart
+++ lib/model.dart
@@ -3,6 +3,15 @@ import 'constants.dart';
 import 'dart:convert';
 import 'dart:async';
 
-class Movie{
+class Movie {
+  final String movieTitle;
+  final String posterPath;
+  final String overview;
 
+  Movie(this.movieTitle, this.posterPath, this.overview);
+
+  Movie.fromJson(Map json)
+      : movieTitle = json['title'],
+        posterPath = json['poster_path'],
+        overview = json['overview'];
 }
