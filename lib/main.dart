d091ac949a619623915cea45188dbe8251dd0ab7
Aditya Soni <adityasoni5859@gmail.com>
2018-10-28 18:07:32 +0530

    feature: Added bloc to ui.

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,56 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final movieProvider = MovieProvider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Some Title'),
       ),
-      body: Text('Some Text'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              onChanged: movieProvider.query.add,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Search for a movie',
+              ),
+            ),
+          ),
+          StreamBuilder(
+            stream: movieProvider.log,
+            builder: (context, snapshot) {
+              return Container(
+                child: Text(snapshot?.data ?? ''),
+              );
+            },
+          ),
+          Flexible(
+            child: StreamBuilder(
+              stream: movieProvider.results,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Image.network('https://image.tmdb.org/t/p/w92' +
+                            snapshot.data[index].posterPath),
+                      ),
+                      title: Text(snapshot.data[index].title),
+                      subtitle: Text(snapshot.data[index].overview),
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
