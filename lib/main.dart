fae9aa9fbfd4e9c2919dda9610c8c084962cc67f
Aditya Soni <adityasoni5859@gmail.com>
2018-10-28 17:54:22 +0530

    feature: Instantiating provider in the main application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_pattern/constants.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_bloc_pattern/api.dart';
+import 'package:flutter_bloc_pattern/bloc.dart';
+import 'package:flutter_bloc_pattern/provider/movie_provider.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,29 +9,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return MovieProvider(
+      movieBloc: MovieBloc(API()),
+      child: new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  Future<String> getFuture() async {
-    String _url =
-        "https://api.themoviedb.org/3/search/company?api_key=${Constants.API_KEY}&query={1}";
-    await http.Client().get(Uri.parse(_url.replaceAll('{1}', 'abc')));
-    return 'This is From Future';
-  }
-
   @override
   Widget build(BuildContext context) {
-    getFuture().then((response) {
-      print(response.toString());
-    });
     return Scaffold(
       appBar: AppBar(
         title: Text('Some Title'),
